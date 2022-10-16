0860061d2bd4e589aa74dd3b1777a2001fa56378
Ajay <VismayaLakshmi.Ajay@philips.com>
2022-10-16 19:58:57 +0530

    initial commit

---
+++ src/test/java/com/howtodoinjava/demo/SpringBootDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.howtodoinjava.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+
+@SpringBootTest(classes= com.howtodoinjava.rest.SpringBootDemoApplication.class)
+public class SpringBootDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
